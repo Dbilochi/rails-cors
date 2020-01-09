32b7e4b13895bf808128e3e9bb24d3e3d83fd103
Deeksha Bilochi <deekshabilochi@Deekshas-MacBook-Pro.local>
2020-01-09 14:21:31 +0530

    cors enable

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::ArticlesController < ApplicationController
         articles = Article.order('created_at DESC');
         render json: {data: articles,message: 'Loaded articles'}
     end
+
+    def create
+        # Create article and display list
+        articles = Article.order('created_at DESC');
+        render json: {data: articles,message: 'Loaded articles'}
+    end
 end
